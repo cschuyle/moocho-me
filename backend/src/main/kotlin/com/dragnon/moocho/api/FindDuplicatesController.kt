f1ff3146fb4f12798649208ee0c0a3da6dd948d3
Carlton Schuyler <carl@dragnon.com>
2024-06-19 16:29:31 -0600

    Make it clear the FindDuplicatescontroller is in-progress

--- backend/src/main/kotlin/com/dragnon/moocho/api/FindDuplicatesController.kt
+++ backend/src/main/kotlin/com/dragnon/moocho/api/FindDuplicatesController.kt
@@ -4,16 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-// TODO These endpoints are not used by the UI.
-
-@RestController
+// TODO These endpoints are not used by the UI. Hence the commented-outness of the following line.
+//@RestController
 class FindDuplicatesController(val repository: TroveRepository) {
 
-    @GetMapping("/duplicates/top")
+//    @GetMapping("/duplicates/top")
     fun getTopDuplicates(@RequestParam minScore: Double) =
         DuplicateFinder(repository.list()).findTopDuplicates(minScore)
 
-    @GetMapping("/duplicates/none")
+//    @GetMapping("/duplicates/none")
     fun getItemsWithNoDuplicates(@RequestParam minScore: Double): MutableList<NoDuplicateDescription> {
         return DuplicateFinder(repository.list()).findItemsWithNoDuplicates(minScore)
     }
