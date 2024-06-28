2efd75e3b705da0d3bb78ae3182db6dfafc49738
Carlton Schuyler <carl@dragnon.com>
2024-06-29 01:39:37 +0200

    Java 17 --> 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ group = "com.dragnon.moocho-web"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
@@ -51,7 +51,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
 }
 
