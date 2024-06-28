d155fbcb1a7a02eb9099469694aae27dcb2b65cf
Carlton Schuyler <carl@dragnon.com>
2024-06-28 10:44:57 +0200

    Add Kotlin-required stuff to Jar tasks in build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,5 +82,15 @@ tasks.withType<Jar> {
     manifest {
         attributes["Main-Class"] = "com.dragnon.moocho.api.AppKt"
     }
+    // To avoid the duplicate handling strategy error
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    // To add all of the dependencies
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
 }
 
