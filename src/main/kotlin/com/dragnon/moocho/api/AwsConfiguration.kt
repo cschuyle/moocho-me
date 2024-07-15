3b691a7962c7afa16599dd480c9303859dcea033
Carlton Schuyler <carl@dragnon.com>
2024-07-14 22:19:01 -0600

    Now I remember how to default to null in SpEL

--- src/main/kotlin/com/dragnon/moocho/api/AwsConfiguration.kt
+++ src/main/kotlin/com/dragnon/moocho/api/AwsConfiguration.kt
@@ -14,6 +14,9 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 class AwsConfiguration {
+
+    private val logger = org.slf4j.LoggerFactory.getLogger(this::class.java)
+
     @Bean
     fun objectMapper(): ObjectMapper {
         return jacksonObjectMapper()
@@ -23,20 +26,22 @@ class AwsConfiguration {
     fun amazonS3Client(
         @Value("\${aws.access.key}") accessKey: String,
         @Value("\${aws.secret.key}") secretKey: String,
-        @Value("\${aws.region:null}") region: String?,
-        @Value("\${aws.endpoint.url:null}") endpoint: String?
+        @Value("\$aws.region:#{null}") region: String?,
+        @Value("\$aws.endpoint.url:#{null}") endpoint: String?
     ): AmazonS3 {
         val creds = AWSStaticCredentialsProvider(BasicAWSCredentials(accessKey, secretKey))
         if (endpoint == null) {
             if (region == null) {
                 throw IllegalStateException("Property aws.endpoint.url not provided, therefore must provide aws.region, which is not provided")
             }
+            logger.info("AWS login to standard AWS")
             return AmazonS3ClientBuilder
                 .standard()
                 .withCredentials(creds)
                 .withRegion(region)
                 .build()
         } else {
+            logger.warn("AWS login to non-standard endpoint")
             return AmazonS3ClientBuilder
                 .standard()
                 .withCredentials(creds)
